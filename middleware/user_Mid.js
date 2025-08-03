399f2329118a86c1bf315422681e00527b0db20f
PyThor97 <pythor97il@gmail.com>
2025-08-03 15:19:21 +0300

    add user authentication middleware with login check and JWT handling

---
+++ middleware/user_Mid.js
@@ -0,0 +1,45 @@
+const md5 = require("md5");
+
+async function CheckLogin(req, res, next) {
+    let uname = req.body.uname || "";
+    let passwd = req.body.passwd || "";
+    let enc_pass = md5("A" + passwd);
+    let Query = `SELECT * FROM users WHERE uname = '${addSlashes(uname)}' AND passwd = '${enc_pass}'`;
+
+    const promisePool = db_pool.promise();
+    let rows = [];
+    try {
+        [rows] = await promisePool.query(Query);
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (rows.length > 0) {
+        req.validUser = true;
+        let val = `${rows[0].id},${rows[0].name}`;
+        const token = jwt.sign({ data: val }, "TaskSecret", { expiresIn: "30d" });
+        res.cookie("TaskToken", token, { maxAge: 30 * 24 * 60 * 60 * 1000 });
+    }
+
+    next();
+}
+
+async function isLogged(req, res, next) {
+    const jwtToken = req.cookies.TaskToken;
+    if (!jwtToken) return res.redirect("/login");
+
+    try {
+        const decoded = jwt.verify(jwtToken, "TaskSecret");
+        const [user_id, name] = decoded.data.split(",");
+        req.user_id = parseInt(user_id);
+        req.user_name = name;
+        next();
+    } catch (err) {
+        return res.redirect("/login");
+    }
+}
+
+module.exports = {
+    CheckLogin,
+    isLogged,
+};
